6770ba88b40b7cfd14e8fe7881e7185e363f0960
Eroxl <InfinityWaffles@outlook.com>
2022-12-20 17:29:36 -0600

    Added getLogElementForScript.ts
    Added getLogElementForScript.ts which gets a script's log element

---
+++ src/gui/helpers/getLogElementForScript.ts
@@ -0,0 +1,21 @@
+/**
+ * Get a scripts log element.
+ * @returns Log element or undefined
+ */
+const getLogElementForScript = (name: string, args: string[]) => {
+  const doc = eval("document") as typeof document;
+
+  const logAreas = [...doc.querySelectorAll(".react-draggable .react-resizable")]
+
+  const logArea = logAreas.find((logArea) => {
+    const logAreaName = logArea.querySelector(".drag")?.textContent;
+
+    if (!logAreaName) return false;
+
+    return logAreaName === `${name} ${args.join(" ")}`;
+  });
+
+  return logArea;
+};
+
+export default getLogElementForScript;
