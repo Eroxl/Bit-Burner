7ff614bce05d689aa84272be52c69b7cb06ba22d
Eroxl <InfinityWaffles@outlook.com>
2022-12-30 18:36:30 -0800

    Start observing new tail windows

--- src/gui/backend/parser.ts
+++ src/gui/backend/parser.ts
@@ -2,26 +2,9 @@ import type { NS } from '../../NetscriptDefinitions';
 
 export function main(ns: NS) {
   // -=- Variables -=-
-  const tailWindows: { [key: string]: HTMLElement } = {};
+  const elements: Record<string, HTMLElement> = {};
 
   // -=- Mutation Observers -=-
-  // ~ Tail window mutation observer
-  // = Create a mutation observer to watch for new tail windows
-  const rootObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      const { addedNodes, removedNodes } = mutation;
-
-      // TODO:EROXL: (2022-12-30) Parse new tail windows
-    });
-  });
-
-  // = If the root element doesn't exist, return
-  if (!document.getElementById('root')) return;
-
-  // = Observe the root element for child list mutations
-  rootObserver.observe(document.getElementById('root')!, {
-    childList: true,
-  });
 
   // ~ Tail window mutation observer
   // = Create a mutation observer to watch for new text in tail windows
@@ -39,6 +22,32 @@ export function main(ns: NS) {
 
   // = Observe the main terminal for child list mutations
   tailObserver.observe(document.getElementById('terminal')!, {
+    childList: true,
+  });
+
+  // ~ Tail window mutation observer
+  // = Create a mutation observer to watch for new tail windows
+  const rootObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const { addedNodes } = mutation;
+
+      // = Check for new tail windows
+      addedNodes.forEach((node) => {
+        if (!(node instanceof HTMLElement)) return;
+
+        // = Start watching for new text in the tail window
+        tailObserver.observe(node, {
+          childList: true,
+        });
+      });
+    });
+  });
+
+  // = If the root element doesn't exist, return
+  if (!document.getElementById('root')) return;
+
+  // = Observe the root element for child list mutations
+  rootObserver.observe(document.getElementById('root')!, {
     childList: true,
   });
 
